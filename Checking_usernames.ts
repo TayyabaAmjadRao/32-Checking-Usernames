1aad9c9ae659fee4d7cde032e57b025ca1951c4d
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:35:41 -0800

    Add files via upload

---
+++ Checking_usernames.ts
@@ -0,0 +1,22 @@
+//Written by :TAYYABA RAO
+//Date: 18/feb/2024
+
+//current user array
+const current_users:string[] = ['Rukhsana','Farzana','Shazia','Nadia','Bushra'];
+
+//new users array
+const new_users:string[] = ['Farzana','Nadia','Adeel','Tanzeel','Bushra'];
+
+//loop through new_users list
+for (let new_user of new_users) {
+    
+    const lowercase_new_users = new_user.toLowerCase();
+
+    if (current_users.map(user => user.toLowerCase()).includes(lowercase_new_users)){
+
+        console.log(`The username '${new_users}' is not avaiable. Please choose a different username.`);
+    } else {
+
+        console.log(`The username '${new_users}' is avaiable.`);
+    }
+}
